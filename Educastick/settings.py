4224ba965bb246ffd8202ae10997b5f88d4e070c
Настя <anastasia.tob@mail.ru>
2023-06-01 21:08:53 +0500

    start

--- Educastick/settings.py
+++ Educastick/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'dbEducastick',
-        'USER': 'username',
+        'USER': 'postgres',
         'PASSWORD': '1',
         'HOST': '127.0.0.1',
         'PORT': '5432'
